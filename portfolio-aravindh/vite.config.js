ebcac114d13aab518c09af024fd249c041ebf932
Aravindh Raj <aravindhrajbca@gmail.com>
2025-09-13 19:29:15 +0530

    Update vite.config.js

--- portfolio-aravindh/vite.config.js
+++ portfolio-aravindh/vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "./",
-  server: {
-    historyApiFallback: true,
-  },
+  plugins: [react()],
+  base: "./", // ensures correct paths for scripts/assets when deployed
 });
